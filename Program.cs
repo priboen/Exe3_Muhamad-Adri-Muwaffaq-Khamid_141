8a5557beb4bf68c183106c3563578e0658eb3e25
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-11-23 15:32:02 +0700

    make circular list class and add Circular list method for declare LAST variable has a null value

--- Program.cs
+++ Program.cs
@@ -13,6 +13,14 @@ namespace Exe3_Muhamad_Adri_Muwaffaq_Khamid_141
         public string name;
         public Node next;
     }
+    class CircularList
+    {
+        Node LAST;
+        public CircularList()
+        {
+            LAST = null;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
