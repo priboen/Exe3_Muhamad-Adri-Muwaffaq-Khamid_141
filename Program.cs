be8c400ee1accc08b5b0722a771a248e6e361de7
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-11-23 15:38:33 +0700

    make method for search data and logical operations for search data

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,14 @@ namespace Exe3_Muhamad_Adri_Muwaffaq_Khamid_141
         public CircularList()
         {
             LAST = null;
+        }
+
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            if (rollNo == LAST.rollNumber)
+                return true;
+            else
+                return false;
         }
     }
     internal class Program
