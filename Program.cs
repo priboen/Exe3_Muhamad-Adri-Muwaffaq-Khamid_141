227101b52ce59671195cd362a8b662202252838f
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-11-23 16:20:26 +0700

    add new menu for add and delete record

--- Program.cs
+++ Program.cs
@@ -77,11 +77,13 @@ namespace Exe3_Muhamad_Adri_Muwaffaq_Khamid_141
                 try
                 {
                     Console.WriteLine("\nMenu");
-                    Console.WriteLine("1. View all the records in the list.");
-                    Console.WriteLine("2. Search for the record in the list.");
-                    Console.WriteLine("3. Display the first record in the list.");
-                    Console.WriteLine("4. Exit");
-                    Console.Write("\nEnter your choice (1-4) : ");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all records in the list");
+                    Console.WriteLine("4. Search for a record in the list");
+                    Console.WriteLine("5. Display the first record in the list");
+                    Console.WriteLine("6. Exit\n");
+                    Console.WriteLine("Enter your choice (1-6): ");
                     char ch = Convert.ToChar(Console.ReadLine());
 
                     switch (ch)
